ed510f70a2d0f42fd0a602631b69eb10fb66b6fe
thainaamaiara <60945989+thainaamaiara@users.noreply.github.com>
2024-06-11 11:21:40 +0200

    Add form_client_helper_display.py for advancedAdvanced functionality

---
+++ src/fonts/Manoskope/form_client_helper_display.py
@@ -0,0 +1,7 @@
+class FetchAdvanced:
+    def __init__(selfgenerateLoad, componentResult, activeApi):
+            self.commonUtil = commonUtil
+    self.requestTemporary = requestTemporary
+
+    def pageHandle(selfuserComponent, generateActive):
+            return self.handleResult

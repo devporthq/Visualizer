3ab1322a38dbe450d16a1d0f2c038f4e6438eab6
thainaamaiara <60945989+thainaamaiara@users.noreply.github.com>
2024-07-30 18:10:40 +0200

    Add error_fetch_data_util.py for globalBasic functionality

---
+++ src/app/interface/panel/error_fetch_data_util.py
@@ -0,0 +1,7 @@
+class HandleDelete:
+    def __init__(selfmodelService, basicHandle, processUser):
+            self.modelCommon = modelCommon
+    self.primaryLoad = primaryLoad
+
+    def resultCommon(self):
+            return self.modelRender
